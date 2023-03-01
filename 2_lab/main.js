0c62f3961640c5251d86ebd395a38c64670af8b8
ilyaevdokimov01 <ilyaevdokimov01@gmail.com>
2023-03-01 10:23:35 +0300

    lab_2

---
+++ 2_lab/main.js
@@ -0,0 +1,63 @@
+function speedConverter(speed, type = "toMS") {
+    let convered = null;
+    let measure_Unit = "м/c";
+
+    if (type === "toMS") {
+        convered = Math.round(speed / 3.6);
+    } else if (type === "toKMH"){
+        convered = Math.round(speed * 3.6);
+        measure_Unit = "км/ч";
+    } else {
+        measure_Unit = null;
+        return false;
+    }
+
+    if (convered && measure_Unit){
+        return `${convered} ${measure_Unit}`;
+    }
+}
+
+function absValue(value) {
+    console.log(value);
+    if (value < 0) {
+        return value * (-1);
+    } else {
+        return value;
+    }
+}
+
+function objectFields() {
+    const student = {
+        group: "211-324",
+        name: "Илья",
+        surname: "Евдокимов"
+    };
+    console.log(Object.keys(student));
+
+    console.log(`Студент ${student.name} ${student.surname} учится в ${student.group} группе`);
+}
+
+
+
+
+
+
+
+
+
+
+let numberEx = Number(prompt('Введите номер задания'));
+
+switch (numberEx){
+    case 1:
+        console.log("Задача №1 Функция конвертации скоростей");
+        console.log(speedConverter(36, "toMS"));
+        console.log(speedConverter(36, "toKMH"));
+        break;
+    case 2:
+        console.log("Задача №2 Абсолютное значение");
+        console.log(absValue(-35));
+    case 3:
+        console.log("Задача №3 Работа с объектом");
+        objectFields();
+}
